0e95c5c1bed95a8b32a699afd954295e4c7de407
repoYan <yan_cristiano@hotmail.com>
2024-01-26 08:52:53 -0500

    Se realizo en el modelo la creación de la clase Estudiante

--- src/main/java/co/edu/uniquindio/registroasignaturas/model/Estudiante.java
+++ src/main/java/co/edu/uniquindio/registroasignaturas/model/Estudiante.java
@@ -1,4 +1,31 @@
 package co.edu.uniquindio.registroasignaturas.model;
 
 public class Estudiante {
+    //Atributos:
+    private String nombre;
+    private String cedula;
+
+    //Constructores:
+    public Estudiante(){}
+    public Estudiante(String nombre, String cedula)
+    {
+        this.nombre = nombre;
+        this.cedula = cedula;
+    }
+    //Getters/setters:
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public void setCedula(String cedula) {
+        this.cedula = cedula;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+    public String getCedula() {
+        return cedula;
+    }
+
 }
