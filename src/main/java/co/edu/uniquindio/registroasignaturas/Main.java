b4d0eac380fa675db0e80ec4da8f0ffe56275a8c
repoYan <yan_cristiano@hotmail.com>
2024-01-30 13:55:36 -0500

    Se creo un paquete para almacenar imagenes para la GUI

--- src/main/java/co/edu/uniquindio/registroasignaturas/Main.java
+++ src/main/java/co/edu/uniquindio/registroasignaturas/Main.java
@@ -17,8 +17,8 @@ public class Main extends Application {
     @Override
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(Main.class.getResource("LoginView.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 320, 240);
-        stage.setTitle("Hello!");
+        Scene scene = new Scene(fxmlLoader.load(), 600, 440);
+        stage.setTitle("Login");
         stage.setScene(scene);
         stage.show();
     }
