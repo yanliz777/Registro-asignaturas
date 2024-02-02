553f05a1ea750dea19417259f7d4ff00c8623c93
repoYan <yan_cristiano@hotmail.com>
2024-02-02 10:33:45 -0500

    Se creo un controlador nuevo para hacer los registros

--- src/main/java/co/edu/uniquindio/registroasignaturas/controllers/ControllerLogin.java
+++ src/main/java/co/edu/uniquindio/registroasignaturas/controllers/ControllerLogin.java
@@ -1,14 +1,42 @@
 package co.edu.uniquindio.registroasignaturas.controllers;
 
+import co.edu.uniquindio.registroasignaturas.Main;
+import co.edu.uniquindio.registroasignaturas.enums.TipoUsuario;
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.ComboBox;
 import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
 
 public class ControllerLogin {
-    @FXML
-    private Label welcomeText;
+
+    Main Ventana = new Main();//para darle manejo a las ventanas
+
+    ControllerRegistro controllerRegistro = new ControllerRegistro();//para comunicarnos con este
 
     @FXML
-    protected void onHelloButtonClick() {
-        welcomeText.setText("Welcome to JavaFX Application!");
+    private ComboBox<TipoUsuario> cbxTypeUserLogin;
+
+    @FXML
+    private TextField txtPasswordLogin;
+
+    @FXML
+    private Button btnLogin;
+
+    @FXML
+    private TextField txtUserLogin;
+
+    @FXML
+    private Button btnRegister;
+
+    @FXML
+    void btnLogin(ActionEvent event) {
+
+    }
+
+    @FXML
+    void btnRegister(ActionEvent event) {
+
     }
 }
---
+++ src/main/java/co/edu/uniquindio/registroasignaturas/controllers/ControllerRegistro.java
@@ -0,0 +1,5 @@
+package co.edu.uniquindio.registroasignaturas.controllers;
+
+public class ControllerRegistro {
+
+}
