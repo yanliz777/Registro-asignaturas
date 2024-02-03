c2cb3f6df7df612379c9e834ea4ec3d49543a625
repoYan <yan_cristiano@hotmail.com>
2024-02-03 14:43:42 -0500

    Se creo un paquete fabrica donde tenemos el modelFactoty que le da manejo al proyecto

---
+++ src/main/java/co/edu/uniquindio/registroasignaturas/fabrica/ModelFactory.java
@@ -0,0 +1,11 @@
+package co.edu.uniquindio.registroasignaturas.fabrica;
+
+import co.edu.uniquindio.registroasignaturas.model.Universidad_mainLogic;
+import co.edu.uniquindio.registroasignaturas.persistencia.Persistencia;
+
+public class ModelFactory
+{
+    private Persistencia persistencia = new Persistencia();
+    public Universidad_mainLogic universidad;
+    
+}
